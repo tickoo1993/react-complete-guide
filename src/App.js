cb40b6283b167956b7fd8e5097a4843db3c594d5
tickoo1993 <kartik.tickoo@gmail.com>
2023-10-05 23:33:17 -0400

    lesson-2 initial setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import Expenses from "./components/Expenses";
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      title: "inurance",
-      amount: "250",
-      date: new Date(),
-    },
-    {
-      title: "inurance-2",
-      amount: "350",
-      date: new Date(),
-    },
-  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>THIS IS A PARAGRAPH!</p>
-      <Expenses items = {expenses}/>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        <li className="concept">
+          <img src="TODO: IMAGE" alt="TODO: TITLE" />
+          <h2>TODO: TITLE</h2>
+          <p>TODO: DESCRIPTION</p>
+        </li>
+      </ul>
     </div>
   );
 }
